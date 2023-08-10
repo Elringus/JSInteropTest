8da0598e74f8f9ef0cebd506283a611177804c9f
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-08-10 19:02:15 +0300

    update

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
-# Repro Steps
+# Environment
+1. Install latest .NET SDK:
+   - https://dotnet.microsoft.com/en-us/download/dotnet/scripts
+   - `Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass`
+   - `./dotnet-install.ps1 -Channel 8.0.1xx -Quality daily`
+   - set `Path` system var to `%AppData%\Local\Microsoft\dotnet\`
+2. Add workload manifest to `nuget.config`:
+   - `<add key="dotnet8" value="https://pkgs.dev.azure.com/dnceng/public/_packaging/dotnet8/nuget/v3/index.json"/>`
+3. Restore workload with `dotnet workload restore`
 
+# Project
 1. Run `dotnet publish`
-2. Copy `dotnet.native.wasm`, `dotnet.native.js` and `dotnet.runtime.js` to `AppBundle/managed`
-3. Run `serve` under `AppBundle` dir
-4. Open the served index page in browser
+2. Rename `AppBundle/_framework` to `AppBundle/bin`
+3. Move `AppBundle/bin/dotnet.js` to `AppBundle/dotnet.js` (otherwise it's implicitly loading native and runtime js modules from the same dir)
+4. Run `serve` under `AppBundle` dir
+5. Open the served index page in browser
--- main.js
+++ main.js
@@ -1,61 +1,55 @@
-import { dotnet } from "./_framework/dotnet.js";
+import { dotnet } from "./dotnet.js";
 
 (async function () {
     const config = {
         mainAssemblyName: "Test.dll",
         assets: [
             {
-                name: "dotnet.js",
-                // buffer: await fetchBinary("dotnet.js"),
-                behavior: "js-module-dotnet"
-            },
-            {
                 name: "dotnet.native.js",
-                // buffer: await fetchBinary("dotnet.native.js"),
+                buffer: await fetchBin("dotnet.native.js"),
                 behavior: "js-module-native"
             },
             {
                 name: "dotnet.runtime.js",
-                // buffer: await fetchBinary("dotnet.runtime.js"),
+                buffer: await fetchBin("dotnet.runtime.js"),
                 behavior: "js-module-runtime"
             },
             {
                 name: "dotnet.native.wasm",
-                // buffer: await fetchBinary("dotnet.native.wasm"),
+                buffer: await fetchBin("dotnet.native.wasm"),
                 behavior: "dotnetwasm"
             },
             {
                 name: "System.Private.CoreLib.wasm",
-                // buffer: await fetchBinary("System.Private.CoreLib.wasm"),
+                buffer: await fetchBin("System.Private.CoreLib.wasm"),
                 behavior: "assembly"
             },
             {
                 name: "System.Runtime.InteropServices.JavaScript.wasm",
-                // buffer: await fetchBinary("System.Runtime.InteropServices.JavaScript.wasm"),
+                buffer: await fetchBin("System.Runtime.InteropServices.JavaScript.wasm"),
                 behavior: "assembly"
             },
             {
                 name: "System.Console.wasm",
-                // buffer: await fetchBinary("System.Console.wasm"),
+                buffer: await fetchBin("System.Console.wasm"),
                 behavior: "assembly"
             },
             {
                 name: "System.Linq.wasm",
-                // buffer: await fetchBinary("System.Linq.wasm"),
+                buffer: await fetchBin("System.Linq.wasm"),
                 behavior: "assembly"
             },
             {
                 name: "Test.wasm",
-                // buffer: await fetchBinary("Test.wasm"),
+                buffer: await fetchBin("Test.wasm"),
                 behavior: "assembly"
             }
-        ],
-        globalizationMode: "invariant"
+        ]
     };
 
     const runtime = await dotnet.withConfig(config).create();
 
-    // setModuleImports("moduleIdCanBeAnything", {
+    // runtime.setModuleImports("moduleIdCanBeAnything", {
     //     getStringAsync: async () => {
     //         await new Promise(res => setTimeout(res, 100));
     //         return "Hello from JS!";
@@ -65,19 +59,19 @@ import { dotnet } from "./_framework/dotnet.js";
     //         getNumberAtAsync: async (index) => {
     //             await new Promise(res => setTimeout(res, 100));
     //             return index;
-    //         },
+    //         }
     //     }
     // });
-
+    //
     // await dotnet.run();
-    // const exports = await getAssemblyExports(config.mainAssemblyName);
-
+    // const exports = await runtime.getAssemblyExports(config.mainAssemblyName);
+    //
     // console.log(exports.Program.SumNumbers());
     // console.log(await exports.Program.SumNumbersAsync(1, 9));
     // console.log(await exports.Program.EchoAsync());
 })();
 
-async function fetchBinary(name) {
-    const uri = `./_framework/${name}`;
-    return new Uint8Array(await (await fetch(uri)).arrayBuffer());
+async function fetchBin(name) {
+    return new Uint8Array(await (await fetch(`./bin/${name}`)).arrayBuffer());
 }
+
