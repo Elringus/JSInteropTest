b6ea161e00accbf5dfca2640e48010b580d0fc93
Artyom Sovetnikov <2056864+Elringus@users.noreply.github.com>
2023-10-08 22:03:47 +0300

    test mt

--- JSInteropTest/Frontend/boot.js
+++ JSInteropTest/Frontend/boot.js
@@ -10,58 +10,65 @@ export async function boot() {
     //     mainAssemblyName: "JSInteropTest.dll",
     //     assets: [
     //         {
-    //             name: "dotnet.runtime.js",
+    //             name: "./bin/dotnet.runtime.js",
     //             // moduleExports: runtimeModule,
     //             behavior: "js-module-runtime"
     //         },
     //         {
-    //             name: "dotnet.native.js",
+    //             name: "./bin/dotnet.native.js",
     //             // moduleExports: nativeModule,
     //             behavior: "js-module-native"
     //         },
     //         {
-    //             name: "dotnet.native.worker.js",
+    //             name: "./bin/dotnet.native.worker.js",
     //             behavior: "js-module-threads"
     //         },
     //         {
-    //             name: "dotnet.native.wasm",
+    //             name: "./bin/dotnet.native.wasm",
     //             // buffer: await fetchBin("dotnet.native.wasm"),
     //             behavior: "dotnetwasm"
     //         },
     //         {
     //             name: "System.Private.CoreLib.wasm",
-    //             buffer: await fetchBin("System.Private.CoreLib.wasm"),
+    //             resolvedUrl: "./_framework/bin/System.Private.CoreLib.wasm",
+    //             // buffer: await fetchBin("System.Private.CoreLib.wasm"),
     //             behavior: "assembly"
     //         },
     //         {
     //             name: "System.Runtime.InteropServices.JavaScript.wasm",
-    //             buffer: await fetchBin("System.Runtime.InteropServices.JavaScript.wasm"),
-    //             behavior: "assembly"
+    //             // buffer: await fetchBin("System.Runtime.InteropServices.JavaScript.wasm"),
+    //             behavior: "assembly",
+    //             resolvedUrl: "./_framework/bin/System.Runtime.InteropServices.JavaScript.wasm"
     //         },
     //         {
     //             name: "System.Console.wasm",
-    //             buffer: await fetchBin("System.Console.wasm"),
-    //             behavior: "assembly"
+    //             // buffer: await fetchBin("System.Console.wasm"),
+    //             behavior: "assembly",
+    //             resolvedUrl: "./_framework/bin/System.Console.wasm"
     //         },
     //         {
     //             name: "System.Linq.wasm",
-    //             buffer: await fetchBin("System.Linq.wasm"),
-    //             behavior: "assembly"
+    //             // buffer: await fetchBin("System.Linq.wasm"),
+    //             behavior: "assembly",
+    //             resolvedUrl: "./_framework/bin/System.Linq.wasm"
     //         },
     //         {
     //             name: "System.Threading.Channels.wasm",
-    //             buffer: await fetchBin("System.Threading.Channels.wasm"),
-    //             behavior: "assembly"
+    //             // buffer: await fetchBin("System.Threading.Channels.wasm"),
+    //             behavior: "assembly",
+    //             resolvedUrl: "./_framework/bin/System.Threading.Channels.wasm"
     //         },
     //         {
     //             name: "OtherAssembly.wasm",
-    //             buffer: await fetchBin("OtherAssembly.wasm"),
-    //             behavior: "assembly"
+    //             // buffer: await fetchBin("OtherAssembly.wasm"),
+    //             behavior: "assembly",
+    //             resolvedUrl: "./_framework/bin/OtherAssembly.wasm"
     //         },
     //         {
     //             name: "JSInteropTest.wasm",
-    //             buffer: await fetchBin("JSInteropTest.wasm"),
-    //             behavior: "assembly"
+    //             // buffer: await fetchBin("JSInteropTest.wasm"),
+    //             behavior: "assembly",
+    //             resolvedUrl: "./_framework/bin/JSInteropTest.wasm"
     //         }
     //     ]
     // };
@@ -70,24 +77,31 @@ export async function boot() {
     const config = {
         mainAssemblyName: "JSInteropTest.dll",
         resources: {
-            jsModuleRuntime: { "./bin/dotnet.runtime.js": "" },
-            jsModuleNative: { "./bin/dotnet.native.js": "" },
-            // jsModuleWorker: { "./bin/dotnet.native.worker.js": "" },
-            wasmNative: { "./bin/dotnet.native.wasm": "" },
-            // wasmSymbols: { "./bin/dotnet.native.js.symbols": "" },
+            jsModuleRuntime: { "dotnet.runtime.js": "" },
+            jsModuleNative: { "dotnet.native.js": "" },
+            jsModuleWorker: { "dotnet.native.worker.js": "" },
+            wasmNative: { "dotnet.native.wasm": "" },
             assembly: {
-                "./bin/System.Private.CoreLib.wasm": "",
-                "./bin/System.Runtime.InteropServices.JavaScript.wasm": "",
-                "./bin/System.Console.wasm": "",
-                "./bin/System.Linq.wasm": "",
-                // "./bin/System.Threading.Channels.wasm": "",
-                "./bin/OtherAssembly.wasm": "",
-                "./bin/JSInteropTest.wasm": "",
+                "System.Private.CoreLib.wasm": "",
+                "System.Runtime.InteropServices.JavaScript.wasm": "",
+                "System.Console.wasm": "",
+                "System.Linq.wasm": "",
+                "System.Threading.Channels.wasm": "",
+                "OtherAssembly.wasm": "",
+                "JSInteropTest.wasm": "",
             }
         }
     };
 
-    const runtime = await dotnet.withExitCodeLogging().withConfig(config).create();
+    const runtime = await dotnet.withConfig(config).withModuleConfig({
+        locateFile: (path, prefix) => {
+            // console.log(`locateFile(path: ${path} prefix: ${prefix})`)
+            return "/_framework/bin/" + path;
+        }
+    }).withResourceLoader((type, name, defaultUri, integrity, behavior) => {
+        // console.log(`withResourceLoader(type: ${type} name: ${name} defaultUri: ${defaultUri} behaviour: ${behavior})`)
+        return "/_framework/bin/" + name;
+    }).create();
     console.log("Runtime created.")
 
     await runtime.runMain("JSInteropTest.dll", []);
